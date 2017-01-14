ece29f004cf783e0d6daba0ff3092740158074d6
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-01-14 21:04:50 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/JoinController.php
+++ app/Http/Controllers/JoinController.php
@@ -2,22 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Org;
 use Toastr;
+
 class JoinController extends Controller
 {
-  public function showPage($id)
-  {
-    $org = Org::find($id);
-    if (!$org){
-      $notification = [
+    public function showPage($id)
+    {
+        $org = Org::find($id);
+        if (!$org) {
+            $notification = [
       'message'    => 'Error! That organization is not in our database!',
       'alert-type' => 'error',
   ];
-      return redirect('')->with($notification);
+
+            return redirect('')->with($notification);
+        }
+        Toastr::info('Test', 'Test Title');
+
+        return view('join')->with('org', $org);
     }
-    Toastr::info('Test', 'Test Title');
-    return view('join')->with('org', $org);
-    }
-  }
+}
