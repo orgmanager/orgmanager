c0d69b5436eb5a1bd594cb90bcf1b42ea03eb94b
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-01-15 21:35:37 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Org;
+use Auth;
 use Illuminate\Http\Request;
 use Toastr;
-use Auth;
 
 class DashboardController extends Controller
 {
@@ -24,31 +24,35 @@ class DashboardController extends Controller
         return view('orgs')->with('orgs', $orgs);
     }
 
-    public function changePassword(Request $request, $id){
-      $org = Org::find($id);
-      if (!$org) {
-          Toastr::error("We couldn\'t find that organization!", 'Error');
+    public function changePassword(Request $request, $id)
+    {
+        $org = Org::find($id);
+        if (!$org) {
+            Toastr::error("We couldn\'t find that organization!", 'Error');
 
-          return redirect('dashboard');
-      }
-      if ($org->userid != Auth::id()){
-        Toastr::error("You tried to edit an organization you don\'t have access to!", 'Auth Error');
-
-        return redirect('dashboard');
-      }
-      if(!$request->has('password') || trim($request->password) == "" || $request->password == $org->password){
-        if ($org->password != "" && $request->password != $org->password){
-          $org->password = null;
-          $org->save();
-          Toastr::success($org->name."\'s password was changed", 'Password Changed');
-          return redirect('dashboard');
+            return redirect('dashboard');
         }
-        Toastr::error("You didn\'t change anything!", 'Error');
+        if ($org->userid != Auth::id()) {
+            Toastr::error("You tried to edit an organization you don\'t have access to!", 'Auth Error');
+
+            return redirect('dashboard');
+        }
+        if (!$request->has('password') || trim($request->password) == '' || $request->password == $org->password) {
+            if ($org->password != '' && $request->password != $org->password) {
+                $org->password = null;
+                $org->save();
+                Toastr::success($org->name."\'s password was changed", 'Password Changed');
+
+                return redirect('dashboard');
+            }
+            Toastr::error("You didn\'t change anything!", 'Error');
+
+            return redirect('dashboard');
+        }
+        $org->password = $request->password;
+        $org->save();
+        Toastr::success($org->name."\'s password was changed", 'Password Changed');
+
         return redirect('dashboard');
-      }
-      $org->password = $request->password;
-      $org->save();
-      Toastr::success($org->name."\'s password was changed", 'Password Changed');
-      return redirect('dashboard');
     }
 }
