7062c0c231f0335ccd46357849c06bbeb96eb452
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-01-19 17:10:56 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- resources/lang/en/alerts.php
+++ resources/lang/en/alerts.php
@@ -12,18 +12,18 @@ return [
     |
     */
 
-    'orgnotfound'   => "We couldn\'t find that organization!",
-    'error'         => 'Error',
-    'captcha'      => "You need to prove you aren\'t a robot!",
-    'captchat'     => 'ReCaptcha required',
-    'username'        => 'You need to submit an username!',
-    'usernamet'     => 'Username required',
-    'passwd1'        => 'You need a password!',
-    'passwdt1'        => 'Password required',
-    'passwd2'        => 'Wrong Password!',
-    'passwdt2'        => 'Wrong Password',
+    'orgnotfound'       => "We couldn\'t find that organization!",
+    'error'             => 'Error',
+    'captcha'           => "You need to prove you aren\'t a robot!",
+    'captchat'          => 'ReCaptcha required',
+    'username'          => 'You need to submit an username!',
+    'usernamet'         => 'Username required',
+    'passwd1'           => 'You need a password!',
+    'passwdt1'          => 'Password required',
+    'passwd2'           => 'Wrong Password!',
+    'passwdt2'          => 'Wrong Password',
     'invite'            => 'We have sent an invite for ',
-    'inbox'           => '. Check your inbox!',
-    'sent'            => 'Invite sent!',
+    'inbox'             => '. Check your inbox!',
+    'sent'              => 'Invite sent!',
 
 ];
