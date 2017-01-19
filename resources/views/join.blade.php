8f2b268a7435a190334a744196a6f56e2be998cf
Miguel Piedrafita <git@miguelpiedrafita.com>
2017-01-19 18:00:47 +0100

    Add Join Translations

--- resources/views/join.blade.php
+++ resources/views/join.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Join {{ $org->name }}</title>
+        <title>@lang('join.join') {{ $org->name }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -20,24 +20,24 @@
             <div class="content">
                 <div class="title m-b-md">
                   <img src="{{ $org->avatar }}" class="logo"><br>
-                    Join {{ $org->name }}
+                    @lang('join.join') {{ $org->name }}
                     @if ($org->description)
                     <blockquote>{{ $org->description }}</blockquote>
                     @endif
                 </div>
                 <div class="join">
-                  Enter your GitHub username @if ($org->password != null && trim($org->password) != "")and the password @endif to join {{ $org->name }}:<br><br>
+                  @lang('join.username') @if ($org->password != null && trim($org->password) != "")@lang('join.passwd') @endif @lang('join.tojoin') {{ $org->name }}:<br><br>
                     <form id="join" method="POST" href="{{ url('join/'.$org->id) }}">
                       {{ csrf_field() }}
-                      <input type="text" name="github_username" class="textbox" placeholder="Your GitHub username"><br><br>
+                      <input type="text" name="github_username" class="textbox" placeholder="@lang('join.uplace')"><br><br>
                       @if ($org->password != null && trim($org->password) != "")
-                      <input type="password" name="org_password" class="textbox" placeholder="password"><br><br>
+                      <input type="password" name="org_password" class="textbox" placeholder="@lang('join.pplace')"><br><br>
                       @endif
                       <center><div class="g-recaptcha" data-sitekey="{{ env('RECAPTCHA_PUBLIC_KEY') }}"></div></center><br><br>
-                      <button type="submit" class="submit-button" name="submit">Join!</button>
+                      <button type="submit" class="submit-button" name="submit">@lang('join.join')!</button>
                     </form>
                 </div>
-                <p class="by">Added by <a href="https://github.com/{{ $org->user->github_username }}" target="_blank">{{ $org->user->github_username }}</a></p>
+                <p class="by">@lang('join.by') <a href="https://github.com/{{ $org->user->github_username }}" target="_blank">{{ $org->user->github_username }}</a></p>
             </div>
         </div>
         <div class="using-github">
