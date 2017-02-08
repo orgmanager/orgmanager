509a722d1419564ce8c4adc929f9c4874fd3cb02
Miguel Piedrafita <git@miguelpiedrafita.com>
2017-02-08 19:49:29 +0100

    Enforce HTTPS in production

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use App\Http\Middleware\HttpsMiddleware;
 
 class Kernel extends HttpKernel
 {
@@ -30,6 +31,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\HttpsMiddleware::class,
         ],
 
         'api' => [
