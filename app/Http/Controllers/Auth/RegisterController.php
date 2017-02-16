41b07702cab8dd5d95e17dea207a464fe487c8f1
Miguel Piedrafita <git@miguelpiedrafita.com>
2017-02-16 20:41:05 +0100

    :bug: Redirect to dashboard if logged-in and visiting login page

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/dashboard';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -12,6 +12,11 @@ use Toastr;
 
 class LoginController extends Controller
 {
+    public function __construct()
+    {
+      $this->middleware('guest', ['except' => 'logoutUser']);
+    }
+
     public function showLogin()
     {
         return view('login');
