38ddfe28f231f183298e7b73673a8c07c862af74
Miguel Piedrafita <git@miguelpiedrafita.com>
2017-02-16 20:22:01 +0100

    :memo: Add all endpoints to the home api endpoint

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -14,6 +14,10 @@ class HomeController extends Controller
         $endpoints->user = url('api/user');
         $endpoints->user_orgs = url('api/user/orgs');
         $endpoints->org = url('api/org/{id}');
+        $endpoints->org_password = url('api/org/{id}');
+        $endpoints->update_org = url('api/org/{id}');
+        $endpoints->join = url('api/join/{id}');
+        $endpoints->stats = url('api/stats');
         $endpoints->docs = url('https://github.com/m1guelpf/orgmanager/wiki').' (TODO)';
 
         return response()->json($endpoints);
