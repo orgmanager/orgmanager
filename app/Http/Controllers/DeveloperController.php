e183eb8dc06f71d959f12134209bc8cfdc1aacdc
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-02-19 16:09:49 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/DeveloperController.php
+++ app/Http/Controllers/DeveloperController.php
@@ -2,33 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
 use Toastr;
-use Auth;
 
 class DeveloperController extends Controller
 {
     public function __construct()
     {
-      $this->middleware('auth', ['except' => 'index']);
+        $this->middleware('auth', ['except' => 'index']);
     }
 
     public function index()
     {
-      return view('developer');
+        return view('developer');
     }
 
     public function token()
     {
-      return view('token');
+        return view('token');
     }
 
     public function deleteToken(Request $request)
     {
-      $user = Auth::user();
-      $user->api_token = str_random(60);
-      $user->save();
-      Toastr::success('Your token has been regenerated successfully', 'Token regenerated!');
-      return redirect()->back();
+        $user = Auth::user();
+        $user->api_token = str_random(60);
+        $user->save();
+        Toastr::success('Your token has been regenerated successfully', 'Token regenerated!');
+
+        return redirect()->back();
     }
 }
