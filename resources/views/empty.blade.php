84838d23862cf227d8b441379dfa138e7a783e5d
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-03-03 12:38:28 +0100

    Change repo URL

--- resources/views/empty.blade.php
+++ resources/views/empty.blade.php
@@ -15,7 +15,7 @@
                         <h3>@lang('empty.heading')</h3>
                         <p>@lang('empty.sync1') <a href="{{ url('sync') }}">@lang('empty.sync2')</a> @lang('empty.sync3')
                         <p>If your app isn't showing here after sync, <a href="https://github.com/settings/connections/applications/10b01d866046f040c9f1">check we've been given access to it</a>.</p>
-                        <p>@lang('empty.problems') <a href="https://github.com/m1guelpf/orgmanager/issues/new?labels=bug" target="_blank">@lang('empty.issue')</a>.</p>
+                        <p>@lang('empty.problems') <a href="https://github.com/orgmanager/orgmanager/issues/new?labels=bug" target="_blank">@lang('empty.issue')</a>.</p>
   </div>
                 </div>
             </div>
