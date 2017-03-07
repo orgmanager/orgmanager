8c0b40d9c20800d59e89f23320c02767e9e6220d
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-03-07 21:21:37 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -2,13 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Org;
 
 class TeamController extends Controller
 {
     public function index(Org $org)
     {
-      return view('teams', ['org' => $org]);
+        return view('teams', ['org' => $org]);
     }
 }
