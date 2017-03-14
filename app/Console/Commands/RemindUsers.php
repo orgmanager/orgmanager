083e16f2712ba7249c44083429805fa18c6ef6fb
Miguel Piedrafita <git@miguelpiedrafita.com>
2017-03-14 21:52:52 +0100

    :sparkles: Send a mail to users that heven't added organizations one week after register  #71

---
+++ app/Console/Commands/RemindUsers.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\User;
+use Carbon\Carbon;
+use App\Mail\RemindUsers as Reminder;
+use Illuminate\Support\Facades\Mail;
+
+class RemindUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'orgmanager:remind-users {{ --force }}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send an emmail to users that have not got any organizations and registered this week. (Internal use)';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $users = User::where('created_at', '>=', Carbon::now()->subWeek())->get();
+        if ($this->option('force') || $this->confirm('Do you want to send emails to '.$users->count().' users?'))
+        {
+          $this->output->progressStart($users->count());
+          foreach ($users as $user)
+          {
+            if (count($user->orgs) == 0)
+            {
+              Mail::to($user->email)->send(new Reminder($user));
+            }
+            $this->output->progressAdvance();
+          }
+          $this->output->progressFinish();
+          $this->info('Successfully sent emails for '.$users->count().' users.');
+        }
+    }
+}
---
+++ app/Mail/RemindUsers.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\User;
+
+class RemindUsers extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('mail.reminder');
+    }
+}
