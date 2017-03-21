a039db5b74df655db85afbc19d96aba7c64b2f36
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-03-21 21:41:21 +0100

    :construction_worker: Add Travis

--- config/database.php
+++ config/database.php
@@ -54,6 +54,18 @@ return [
             'engine'    => null,
         ],
 
+        'travis' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_TEST_HOST', 'localhost'),
+            'database'  => env('DB_TEST_DATABASE', 'travis'),
+            'username'  => env('DB_TEST_USERNAME', 'homestead'),
+            'password'  => env('DB_TEST_PASSWORD', 'secret'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+
         'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => env('DB_HOST', '127.0.0.1'),
