7f2a72c51faefeeac453c40eaf3cadd6a203d794
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-04-08 21:39:46 +0200

    Add steps to follow

--- app/Http/Controllers/AutoJoinerController.php
+++ app/Http/Controllers/AutoJoinerController.php
@@ -10,7 +10,12 @@ class AutoJoinerController extends Controller
     {
         abort_unless($this->requestSignatureIsValid(), 403);
 
-        //
+        // Check X-Github-Event header equals to pull_request (CONSIDER checking against integration_installation for analytics)
+        // Get organization name (name: $json->pull_request->base->repo->owner->login id: pull_request->base->repo->owner->id)
+        // Check organization is registered
+        // Check the PR was merged ($json->action equals merged)
+        // Get github username of the user to invite ($json->pull_request->user->login)
+        // Invite user to organization
     }
 
     protected function requestSignatureIsValid() : bool
