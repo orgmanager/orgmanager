f33757fb96e5db40f24e0dcc3cc5b116f0f11920
Miguel Piedrafita <soy@miguelpiedrafita.com>
2017-04-21 11:08:01 +0000

    Remove junk

--- .probot.js
+++
@@ -1,5 +0,0 @@
-// Auto-respond to new issues and pull requests
-on('issues.opened', 'pull_request.opened')
-  .comment('Thanks for your contribution @{{ user.login }}! We'll try to reply as soon as we can. Meanwhile, [join the OrgManager Organization if you haven\'t!][joinorg]
-  
-  [config]: https://orgmanager.miguelpiedrafita.com/o/orgmanager');
