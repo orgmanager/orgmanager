dd89fabc87247520267c26e8f142c5b696a17a58
Miguel Piedrafita <git@miguelpiedrafita.com>
2017-04-23 17:42:00 +0200

    :sparkles: Add a helpers file

---
+++ app/helpers.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\HtmlString;
+use Parsedown;
+
+if (! function_exists('markdown')) {
+    /**
+     * Parse a Markdown formatted string into HTML.
+     *
+     * @param  null|string  $input
+     * @return \Parsedown|\Illuminate\Support\HtmlString
+     */
+    function markdown($input = null)
+    {
+        $parsedown = app(Parsedown::class);
+        if (func_num_args() == 0) {
+            return $parsedown;
+        }
+        return new HtmlString($parsedown->text($input));
+    }
