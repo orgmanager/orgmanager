5d805f290d16fea4b4ddcfb48e9b1f2563a0d45f
Miguel Piedrafita <git@miguelpiedrafita.com>
2017-04-23 19:06:00 +0200

    :sparkles: Use HTML on SweetAlert
     #48

--- resources/views/join.blade.php
+++ resources/views/join.blade.php
@@ -76,7 +76,7 @@
         @endif
         @if (session('success'))
         <script>
-        swal("Good job!", "{{ session('success') }}", "success")
+        swal({title: "Good job!", text: '{{ session('success') }}', type: "success", html:true})
         </script>
         @endif
         @include('layouts.code.footer')
