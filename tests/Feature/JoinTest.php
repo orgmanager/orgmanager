75512f30f5fbf4d01ad7fd0a4fe1122f8ce2ff6c
Miguel Piedrafita <soy@miguelpiedrafita.com>
2017-04-26 09:12:06 +0000

    Fix status code

--- tests/Feature/JoinTest.php
+++ tests/Feature/JoinTest.php
@@ -25,6 +25,6 @@ class JoinTest extends TestCase
 
         $response = $this->post('join/'.$org->id, ['github_username' => 'idonotexist9995964']);
 
-        $response->assertStatus(301);
+        $response->assertStatus(302);
     }
 }
