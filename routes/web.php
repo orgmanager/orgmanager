a483299a71458d6bca044e2d4b79ad19f08c1277
Miguel Piedrafita <soy@miguelpiedrafita.com>
2017-09-05 14:12:20 +0200

    :sparkles: Name routes

--- routes/web.php
+++ routes/web.php
@@ -13,25 +13,25 @@
 */
 
 // Pages
-Route::get('/', 'HomeController@index');
-Route::get('dashboard', 'DashboardController@index');
-Route::get('org/{org}', 'OrgController@index');
-Route::post('org/{org}', 'OrgController@changePassword');
-Route::put('org/{org}', 'OrgController@update');
-Route::delete('org/{org}', 'OrgController@delete');
-Route::post('org/{org}/message', 'OrgController@message');
-Route::get('org/{org}/teams', 'TeamController@index');
-Route::post('org/{org}/teams', 'TeamController@syncTeams');
-Route::put('org/{org}/teams', 'TeamController@setTeam');
-Route::delete('org/{org}/teams', 'TeamController@deleteTeams');
-Route::get('sync', 'GithubController@syncOrgs');
-Route::post('sync/{org}', 'GithubController@syncOrg');
-Route::get('join/{org}', 'JoinController@index');
-Route::post('join/{org}', 'JoinController@inviteUser');
-Route::get('o/{name}', 'JoinController@redirect');
-Route::get('developer', 'DeveloperController@index');
-Route::get('token', 'DeveloperController@token');
-Route::delete('token', 'DeveloperController@deleteToken');
+Route::get('/', 'HomeController@index')->name('landing');
+Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+Route::get('org/{org}', 'OrgController@index')->name('org');
+Route::post('org/{org}', 'OrgController@changePassword')->name('org.password');
+Route::put('org/{org}', 'OrgController@update')->name('org.update');
+Route::delete('org/{org}', 'OrgController@delete')->name('org.delete');
+Route::post('org/{org}/message', 'OrgController@message')->name('org.message');
+Route::get('org/{org}/teams', 'TeamController@index')->name('org.teams');
+Route::post('org/{org}/teams', 'TeamController@syncTeams')->name('org.teams.sync');
+Route::put('org/{org}/teams', 'TeamController@setTeam')->name('org.teams.set');
+Route::delete('org/{org}/teams', 'TeamController@deleteTeams')->name('org.teams.delete');
+Route::get('sync', 'GithubController@syncOrgs')->name('sync');
+Route::post('sync/{org}', 'GithubController@syncOrg')->name('sync.org');
+Route::get('join/{org}', 'JoinController@index')->name('join');
+Route::post('join/{org}', 'JoinController@inviteUser')->name('join.post');
+Route::get('o/{name}', 'JoinController@redirect')->name('redirect');
+Route::get('developer', 'DeveloperController@index')->name('developer');
+Route::get('token', 'DeveloperController@token')->name('token');
+Route::delete('token', 'DeveloperController@deleteToken')->name('token.delete');
 
 // Auth routes
 Route::get('login', 'LoginController@authorizeUser')->name('login');
