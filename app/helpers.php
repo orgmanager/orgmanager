f5cb599fec37da882a15df82b57240cb1490521e
Miguel Piedrafita <soy@miguelpiedrafita.com>
2017-09-05 14:45:37 +0200

    :sparkles: Switch to CommonMark for Markdown parsing

--- app/helpers.php
+++ app/helpers.php
@@ -1,22 +1,16 @@
 <?php
 
-use Parsedown as Parser;
-use Illuminate\Support\HtmlString;
+use App\Classes\Markdown;
 
 if (! function_exists('markdown')) {
     /**
      * Parse a Markdown formatted string into HTML.
      *
-     * @param  null|string  $input
-     * @return \Parsedown|\Illuminate\Support\HtmlString
+     * @param  string  $input
+     * @return string
      */
-    function markdown($input = null)
+    function markdown(string $markdown) : string
     {
-        $parsedown = app(Parser::class);
-        if (func_num_args() == 0) {
-            return $parsedown;
-        }
-
-        return new HtmlString($parsedown->text($input));
+        return Markdown::render($markdown);
     }
 }
