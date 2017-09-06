ca86deae6555bbc27a528c3c22bf8b3ad7cc7495
Miguel Piedrafita <soy@miguelpiedrafita.com>
2017-09-06 15:40:04 +0200

    :bug: Move the sync route to POST

--- resources/views/empty.blade.php
+++ resources/views/empty.blade.php
@@ -16,7 +16,8 @@
                         <p>You can try to sync the data from GitHub, using the sync button that you have below.</p>
                         <p>If your app isn't showing here after sync, <a href="https://github.com/settings/connections/applications/10b01d866046f040c9f1">check we've been given access to it</a>.</p>
                         <p>@lang('empty.problems') <a href="https://github.com/orgmanager/orgmanager/issues/new?labels=bug" target="_blank">@lang('empty.issue')</a>.</p>
-                        <form id="sync-orgs" method="GET" action="{{ url('sync') }}">
+                        <form id="sync-orgs" method="POST" action="{{ url('sync') }}">
+                          {{ csrf_field() }}
                           <button type="submit" class="btn btn-primary"><i class="octicon octicon-sync"></i> Sync your Organizations</button>
                         </form>
                       </div>
