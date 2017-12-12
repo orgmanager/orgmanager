f6a7ef9179aabde4357b8abf980311876455f5aa
Miguel Piedrafita <soy@miguelpiedrafita.com>
2017-12-12 21:43:23 +0100

    Redesign empty page

--- resources/views/empty.blade.php
+++ resources/views/empty.blade.php
@@ -1,36 +1,31 @@
-@extends('layouts.app')
+@extends('layouts.new')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Organizations</div>
-
-                    <div class="panel-body">
-                        <table>
-                            <tbody>
-                            <div class="blankslate">
-                                <span class="mega-octicon octicon-telescope blankslate-icon"></span>
-                                <h3>@lang('empty.heading')</h3>
-                                <p>You can try to sync the data from GitHub, using the sync button that you have
-                                    below.</p>
-                                <p>If your app isn't showing here after sync, <a
-                                            href="https://github.com/settings/connections/applications/10b01d866046f040c9f1">check
-                                        we've been given access to it</a>.</p>
-                                <p>@lang('empty.problems') <a
-                                            href="https://github.com/orgmanager/orgmanager/issues/new?labels=bug"
-                                            target="_blank">@lang('empty.issue')</a>.</p>
-                                <form id="sync-orgs" method="POST" action="{{ url('sync') }}">
-                                    {{ csrf_field() }}
-                                    <button type="submit" class="btn btn-primary"><i class="octicon octicon-sync"></i>
-                                        Sync your Organizations
-                                    </button>
-                                </form>
-                            </div>
+    <div>
+        <div class="flex items-center justify-center">
+            <svg class="h-16 w-16 text-black-dark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M507.3 484.5L352 328.1a200 200 0 1 0-22.4 22.8l155.2 156.3a16 16 0 0 0 22.6-22.7zm-309-116.2a168.3 168.3 0 1 1 0-336.6 168.3 168.3 0 0 1 0 336.6z"/>
+            </svg>
+        </div>
+        <div class="mt-15">
+            <p class="text-black-light text-2xl m-4 text-center">Looks like there's nothing here</p>
+            <p class="text-base text-black-darker mb-2 text-center">Try to  sync the from your Github account, using the sync button below.</p>
+            <p class="text-base text-black-darker text-center">If your  organization isn't showing here after sync, check <a class="no-underline text-inherit link-shadow link-transition" href="https://github.com/settings/connections/applications/{{ config('services.github.client_id') }}" target="_blank">we’ve been given access to it</a>.</p>
+        </div>
+        <div class="mt-8 text-center">
+            <form action="{{ route('sync') }}" method="POST">
+                {{ csrf_field() }}
+                <button type="submit" class="bg-brand hover:bg-brand-dark text-white font-bold py-4 px-8 rounded-full focus:outline-none">
+                    <div class="inline-block">
+                        <div class="flex items-center justify-center">
+                            <svg class="w-4 h-4 mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path d="M14.7 15.7A8 8 0 1 1 17 10h-2a6 6 0 1 0-1.8 4.2l1.5 1.5zM12 10h8l-4 4-4-4z"/>
+                            </svg>
+                            <span>Sync your organization</span>
+                        </div>
                     </div>
-                </div>
-            </div>
+                </button>
+            </form>
         </div>
     </div>
 @endsection
