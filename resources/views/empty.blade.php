b15c756dcb5a30dde9a2b6e3a2c1fffcf20dd6bc
Miguel Piedrafita <soy@miguelpiedrafita.com>
2017-12-13 21:22:05 +0100

    Tweak body copy in empty page

--- resources/views/empty.blade.php
+++ resources/views/empty.blade.php
@@ -10,7 +10,7 @@
         <div class="mt-15">
             <p class="text-black-light text-2xl m-4 text-center">Looks like there's nothing here</p>
             <p class="text-base text-black-darker mb-2 text-center">Try to  sync the from your Github account, using the sync button below.</p>
-            <p class="text-base text-black-darker text-center">If your  organization isn't showing here after sync, check <a class="no-underline text-inherit link-shadow link-transition" href="https://github.com/settings/connections/applications/{{ config('services.github.client_id') }}" target="_blank">we’ve been given access to it</a>.</p>
+            <p class="text-base text-black-darker text-center">If your  organizations aren't showing here after sync, check <a class="no-underline text-inherit link-shadow link-transition" href="https://github.com/settings/connections/applications/{{ config('services.github.client_id') }}" target="_blank">we’ve been given access to them</a>.</p>
         </div>
         <div class="mt-8 text-center">
             <form action="{{ route('sync') }}" method="POST">
@@ -21,7 +21,7 @@
                             <svg class="w-4 h-4 mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                 <path d="M14.7 15.7A8 8 0 1 1 17 10h-2a6 6 0 1 0-1.8 4.2l1.5 1.5zM12 10h8l-4 4-4-4z"/>
                             </svg>
-                            <span>Sync your organization</span>
+                            <span>Sync your organizations</span>
                         </div>
                     </div>
                 </button>
