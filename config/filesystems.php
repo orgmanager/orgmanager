3830c52f9501054cd9eedcea35a759d89c861bc1
terixte <44103879+terixte@users.noreply.github.com>
2018-11-01 23:51:45 -0400

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,11 +41,7 @@ return [
     |
     */
 
-    'disks' => [
-
-        'local' => [
-            'driver' => 'local',
-            'root'   => storage_path('app'),
+   
         ],
 
         'public' => [
