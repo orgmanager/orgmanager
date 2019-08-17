cb870deb9fd9df5b580dd3a36e51f37aad175642
Miguel Piedrafita <soy@miguelpiedrafita.com>
2019-08-17 19:04:50 +0200

    Create an axios module

---
+++ resources/js/lib/axios.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found');
+}
+
+export default axios
