3ad7b2f64f38645da374a65064b0ed6744633745
Miguel Piedrafita <soy@miguelpiedrafita.com>
2019-08-17 19:37:42 +0200

    Update design of danger zone

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -40,19 +40,15 @@
                 </button>
             </form>
         </div>
-        <div class="bg-white shadow-md rounded-lg p-4 mt-6">
-            <div class="flex items-center justify-between mb-4">
-                <p class="text-xl text-grey-darkest">Remove Organization</p>
-                <p class="text-sm text-grey-darker">This is some helper text to explain what this thing right below does. It's long & stuff.</p>
-            </div>
-
+        <div class="bg-red-lightest shadow-md rounded-lg p-4 mt-6 flex items-center justify-between">
+            <p class="text-xl text-red-darkest">Danger Zone</p>
             <form method="POST" action="{{ route('org.delete', $org) }}" class="text-center">
                 @csrf
                 @method('DELETE')
                 <button type="submit" class="bg-red hover:bg-red-dark text-white font-bold py-3 px-4 rounded-full focus:outline-none">
                     <div class="flex items-center justify-center">
                         <svg class="w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
-                        <span>Delete Organization</span>
+                        <span>Remove Organization</span>
                     </div>
                 </button>
             </form>
