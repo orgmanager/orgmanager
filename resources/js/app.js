ce687f6ff724f9d48932159b78d7156cd084d1d4
Miguel Piedrafita <soy@miguelpiedrafita.com>
2019-08-17 19:02:27 +0200

    Add Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,7 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import Vue from 'vue'
 import swal from 'sweetalert'
 
 window.swal = swal
+new Vue({
+    el: '#app'
+})
